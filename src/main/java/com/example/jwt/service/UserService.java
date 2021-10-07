5080829326788ab0b683e988d4f13f13579ab773
atestelin <adil.testelin59@hotmail.fr>
2021-10-07 17:58:09 +0200

    Début du cleaning du code

--- src/main/java/com/example/jwt/service/UserService.java
+++ src/main/java/com/example/jwt/service/UserService.java
@@ -3,6 +3,9 @@ package com.example.jwt.service;
 import com.example.jwt.domain.Role;
 import com.example.jwt.domain.User;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -11,4 +14,5 @@ public interface UserService {
     void addRoleToUser(String username, String roleName);
     User getUser(String username);
     List<User> getUsers();
+    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
--- src/main/java/com/example/jwt/service/UserServiceImpl.java
+++ src/main/java/com/example/jwt/service/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.jwt.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.jwt.domain.Role;
 import com.example.jwt.domain.User;
 import com.example.jwt.repository.RoleRepository;
 import com.example.jwt.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,10 +19,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Service
 @RequiredArgsConstructor
@@ -75,5 +86,39 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     public List<User> getUsers() {
         log.info("Fetching all users");
         return userRepository.findAll();
+    }
+
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refreshToken = authorizationHeader.substring("Bearer ".length()); // On recupère notre JWT.
+                Algorithm algo = Algorithm.HMAC256("secret".getBytes()); // On réinstancie l'algo qui permettra cette fois de le décoder.
+                JWTVerifier verifier = JWT.require(algo).build();
+                DecodedJWT decodedJWT = verifier.verify(refreshToken); // On décode notre JWT.
+                String username = decodedJWT.getSubject();
+                User user = this.getUser(username);
+                String accessToken = JWT.create()
+                        .withSubject(user.getUsername()) // Unique identificateur, ici on choisit le Username
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 1 * 60 * 1000)) // On set ici une date d'expiration.
+                        .withIssuer(request.getRequestURI().toString()) // Créateur du token, ici je prends la request uri
+                        .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList())) // La liste des droits propres à l'utilisateur.
+                        .sign(algo); // L'algorithme de signature, qui va sécuriser et rendre unique notre token.
+                Map<String, String> mapTokens = new HashMap<>();
+                mapTokens.put("access_token", accessToken);
+                mapTokens.put("refresh_token", refreshToken);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), mapTokens); // On met nos tokens dans une map, que l'on retournera en responseBody si l'authent s'est bien passée.
+            } catch (Exception e) {
+                log.error("Error logging in {}", e.getMessage());
+                response.setHeader("error", e.getMessage());
+                Map<String, String> errors = new HashMap<>();
+                errors.put("error_message", e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), errors);
+            }
+        } else {
+            throw new RuntimeException("Refresh token is missing");
+        }
     }
 }
